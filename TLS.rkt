b7a277d68764b27912ded7f5acfefcc32bcbd20f
Grant Crofton <grantac@hotmail.com>
2015-05-20 08:57:20 +0100

    Up to p.183

--- TLS.rkt
+++ TLS.rkt
@@ -590,7 +590,7 @@
 ;(numbered? 'hi)
 ;(numbered? '((1 + 1) ^ (2 * (3 ^ 4))))
 
-(define value
+(define value_
   (lambda (exp)
     (cond
       ((atom? exp) exp)
@@ -1520,5 +1520,71 @@
        (atom-to-action e)) 
       (else (list-to-action e))))) 
 
-(define (atom-to-action a)
-  ())
+; *const *quote *identifier *lambda *cond and *application.
+
+(define atom-to-action 
+  (lambda (e) 
+    (cond 
+      ((number? e) *const) 
+      ((eq? e #t) *const) 
+      ((eq? e #f) *const) 
+      ((eq? e (quote cons)) *const) 
+      ((eq? e (quote car)) *const) 
+      ((eq? e (quote cdr)) *const) 
+      ((eq? e (quote null?)) *const) 
+      ((eq? e (quote eq?)) *const) 
+      ((eq? e (quote atom?)) *const) 
+      ((eq? e (quote zero?)) *const) 
+      ((eq? e (quote add1)) *const) 
+      ((eq? e (quote sub1)) *const) 
+      ((eq? e (quote number?)) *const) 
+      (else *identifier)))) 
+
+(define list-to-action 
+  (lambda (e) 
+    (cond 
+      ((atom? (car e)) 
+       (cond 
+         ((eq? (car e) (quote quote)) *quote) 
+         ((eq? (car e) (quote lambda)) *lambda) 
+         ((eq? (car e) (quote cond)) *cond) 
+         (else *application))) 
+      (else *application)))) 
+
+(define value 
+  (lambda (e) 
+    (meaning e (quote ())))) 
+
+(define meaning 
+  (lambda (e table) 
+    ((expression-to-action e) e table))) 
+
+(define *const 
+  (lambda (e table) 
+    (cond 
+      ((number? e) e) 
+      (( eq? e #t) #t) 
+      ((eq? e #f) #f) 
+      (else (build (quote primitive) e)))))
+
+(define *quote 
+  (lambda (e table) 
+    (text-of e)))
+
+(define text-of second)
+
+(define *identifier 
+  (lambda (e table) 
+    ((lookup-in-table e table (lambda (name) name)))))
+
+(define *application 
+  (lambda (e table) 
+    (text-of e)))
+
+(define *lambda 
+  (lambda (e table) 
+    (text-of e)))
+
+(define *cond
+  (lambda (e table) 
+    (text-of e)))
